0755e20340fc1590e91f82f88b0552bac00d77eb
Nils Schumann <n.schumann@21torr.com>
2022-10-20 19:03:02 +0200

    Setup gradle for unit tests

---
+++ usersloader/src/test/java/TestTest.kt
@@ -0,0 +1,9 @@
+import com.example.usersloader.UsersLoader
+import org.junit.jupiter.api.Test
+
+class TestTest {
+
+    @Test fun `should provide a list of users`() {
+        val loader = UsersLoader()
+    }
+}
