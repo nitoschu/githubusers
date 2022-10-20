71c251b05b4abea51ed1f3402fb88c357135f65d
Nils Schumann <n.schumann@21torr.com>
2022-10-20 20:25:03 +0200

    Setup Compose

--- app/src/main/java/com/example/githubusers/OverviewFragment.kt
+++ app/src/main/java/com/example/githubusers/OverviewFragment.kt
@@ -1,6 +1,30 @@
 package com.example.githubusers
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 
 class OverviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = ComposeView(requireContext()).apply {
+        setContent {
+            MaterialTheme {
+                OverviewScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun OverviewScreen() {
+        Text("Hello, World")
+    }
 }
