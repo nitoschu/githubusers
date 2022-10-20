601c9480440fc3e333574eb9f99a79258fea3236
Nils Schumann <n.schumann@21torr.com>
2022-10-20 19:45:40 +0200

    Obtain a dummy user

--- usersloader/src/main/java/com/example/usersloader/UsersLoader.kt
+++ usersloader/src/main/java/com/example/usersloader/UsersLoader.kt
@@ -1,4 +1,16 @@
 package com.example.usersloader
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+
 class UsersLoader {
+
+    val users = MutableSharedFlow<String>()
+
+    fun requestUsers(scope: CoroutineScope) {
+        scope.launch {
+            users.emit("Gerd")
+        }
+    }
 }
--- usersloader/src/test/java/TestTest.kt
+++ usersloader/src/test/java/TestTest.kt
@@ -1,9 +1,29 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 import com.example.usersloader.UsersLoader
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class TestTest {
 
-    @Test fun `should provide a list of users`() {
+    @Test
+    fun `should provide a list of users`() = runTest {
         val loader = UsersLoader()
+        val users = mutableListOf<String>()
+
+        val collectJob = launch(UnconfinedTestDispatcher()) {
+            loader.users.toList(users)
+        }
+
+        loader.requestUsers(this)
+        advanceUntilIdle()
+        assertEquals("Gerd", users[0])
+        collectJob.cancel()
     }
 }
