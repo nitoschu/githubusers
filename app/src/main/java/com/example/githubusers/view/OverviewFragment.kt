1b20b4950073aee496f109a92635a4c7469c7342
Nils Schumann <n.schumann@21torr.com>
2022-10-26 22:16:51 +0200

    Make user list item more beautiful

--- app/src/main/java/com/example/githubusers/view/OverviewFragment.kt
+++ app/src/main/java/com/example/githubusers/view/OverviewFragment.kt
@@ -4,23 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.* // ktlint-disable no-wildcard-imports
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import com.example.githubusers.R
 import com.example.githubusers.repository.room.StorableGithubUser
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -43,10 +57,15 @@ class OverviewFragment : Fragment() {
                     uiState = viewModel.uiState.collectAsState(),
                     pagingUsersData = viewModel.users,
                     retryLoadingUsers = { viewModel.retryCollectingUsers() },
-                    onErrorShown = { viewModel.onErrorShown() }
+                    onErrorShown = { viewModel.onErrorShown() },
+                    onUserClick = { navigate() }
                 )
             }
         }
+    }
+
+    private fun navigate() {
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -60,7 +79,8 @@ fun OverviewScreen(
     uiState: State<OverviewUiState>,
     pagingUsersData: Flow<PagingData<StorableGithubUser>>,
     retryLoadingUsers: () -> Unit,
-    onErrorShown: () -> Unit
+    onErrorShown: () -> Unit,
+    onUserClick: () -> Unit
 ) {
 
     val users: LazyPagingItems<StorableGithubUser> = pagingUsersData.collectAsLazyPagingItems()
@@ -72,6 +92,7 @@ fun OverviewScreen(
                 users = users,
                 persistedUsers = uiState.value.persistedUsers,
                 isRefreshing = uiState.value.isLoading,
+                onUserClick = onUserClick,
                 onRefresh = {
                     retryLoadingUsers()
                     users.refresh()
@@ -103,7 +124,8 @@ fun GithubUsersList(
     users: LazyPagingItems<StorableGithubUser>,
     persistedUsers: List<StorableGithubUser>?,
     isRefreshing: Boolean,
-    onRefresh: () -> Unit
+    onRefresh: () -> Unit,
+    onUserClick: () -> Unit,
 ) {
     SwipeRefresh(
         state = rememberSwipeRefreshState(isRefreshing),
@@ -114,11 +136,11 @@ fun GithubUsersList(
         } else {
             if (persistedUsers != null) {
                 LazyColumn() {
-                    items(persistedUsers) { User(it) }
+                    items(persistedUsers) { User(it, onUserClick) }
                 }
             } else {
                 LazyColumn() {
-                    items(users) { it?.let { it1 -> User(it1) } }
+                    items(users) { it?.let { it1 -> User(it1, onUserClick) } }
                 }
             }
         }
@@ -126,9 +148,32 @@ fun GithubUsersList(
 }
 
 @Composable
-fun User(user: StorableGithubUser) {
-    Column {
-        Text(user.login)
+fun User(
+    user: StorableGithubUser,
+    onClick: () -> Unit
+) {
+    Column(Modifier.clickable { /*nav.navigate()*/ }) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(72.dp)
+                    .padding(8.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+                    .background(color = Color.Blue)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(user.login, fontSize = 20.sp)
+        }
+        Divider()
     }
 }
 
@@ -141,4 +186,10 @@ fun EmptyScreen(onRefresh: () -> Unit) {
             Text("Refresh")
         }
     }
+}
+
+@Preview(widthDp = 400, heightDp = 100, showBackground = true)
+@Composable
+fun UserPreview() {
+    User(StorableGithubUser(1234, "Gerhard", "---"), onClick = {})
 }
